92898b0de1914e5e9a82673712f08ec0571fd0f8
BLY <bertri.lorente@gmail.com>
2023-04-07 07:19:57 -0400

    Añadida la funcion de guardar API-KEY

--- popup.js
+++ popup.js
@@ -33,14 +33,19 @@ document.addEventListener('DOMContentLoaded', function() {
   const submitBtn = document.getElementById('submit-btn');
   const apiKeyInput = document.getElementById('api-key-input');
   const emailInput = document.getElementById('email-input');
+  const saveApiKeyCheckbox = document.getElementById('save-api-key-checkbox');
 
-  chrome.runtime.sendMessage({ action: 'getApiKey' }, function(response) {
-    apiKeyInput.value = response.apiKey;
+  chrome.storage.local.get(['apiKey'], function(result) {
+    if (result.apiKey) {
+      apiKeyInput.value = result.apiKey;
+      saveApiKeyCheckbox.checked = true;
+    }
   });
 
   submitBtn.addEventListener('click', function() {
     const email = emailInput.value.trim();
     const apiKey = apiKeyInput.value.trim();
+    const saveApiKey = saveApiKeyCheckbox.checked;
 
     if (email && apiKey) {
       fetchBreachData(email, apiKey)
@@ -49,19 +54,30 @@ document.addEventListener('DOMContentLoaded', function() {
             showResults(breaches);
           } else {
             const tableBody = document.getElementById('breach-results');
-            tableBody.innerHTML = '';
-            errorMessage.innerHTML = 'No se encontraron resultados para el correo electrónico ingresado.';
-          }
-        })
-        .catch(error => {
-          const tableBody = document.getElementById('breach-results');
-          tableBody.innerHTML = '';
-          errorMessage.innerHTML = 'Ocurrió un error al buscar información del correo electrónico ingresado. Por favor, verifica la API key e intenta de nuevo.';
-        });
-    } else {
-      const tableBody = document.getElementById('breach-results');
-      tableBody.innerHTML = '';
-      errorMessage.innerHTML = 'Por favor, ingresa un correo electrónico y una API key válida.';
-    }
-  });
+tableBody.innerHTML = '';
+errorMessage.innerHTML = 'No se encontraron resultados para el correo electrónico ingresado.';
+}
+})
+.catch(error => {
+const tableBody = document.getElementById('breach-results');
+tableBody.innerHTML = '';
+errorMessage.innerHTML = 'Ocurrió un error al buscar información del correo electrónico ingresado. Por favor, verifica la API key e intenta de nuevo.';
 });
+  if (saveApiKey) {
+    chrome.storage.local.set({ 'apiKey': apiKey }, function() {
+      console.log('API key saved.');
+    });
+  } else {
+    chrome.storage.local.remove('apiKey', function() {
+      console.log('API key removed.');
+    });
+  }
+} else {
+  const tableBody = document.getElementById('breach-results');
+  tableBody.innerHTML = '';
+  errorMessage.innerHTML = 'Por favor, ingresa un correo electrónico y una API key válida.';
+}
+
+});
+});
+
